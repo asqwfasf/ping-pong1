04e1a9d9597a1cbb1ce8e36cafb397b198dac49b
asqwfasf <147615962+asqwfasf@users.noreply.github.com>
2023-10-12 16:42:39 +0500

    Update s1.py
    V2

--- s1.py
+++ s1.py
@@ -1,5 +1,32 @@
 from pygame import *
 
+class GameSprite(sprite.Sprite):
+   #конструктор класса
+   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+       super().__init__()
+       # каждый спрайт должен хранить свойство image - изображение
+       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+       self.speed = player_speed
+       # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+#класс-наследник для спрайта-игрока (управляется стрелками)
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+    def fire(self):
+        bullet = Bullet('bullet.png', self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
+
 #Игровая сцена:
 back = (200, 255, 255)
 win_width = 700
